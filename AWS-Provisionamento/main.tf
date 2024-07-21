4980d592920c5ff6537b0f946d09d9ac58e03597
HELBER PATRICK TELES <HELBER.TELES@ibm.com>
2024-07-21 19:28:32 -0300

    Upload terraform

---
+++ AWS-Provisionamento/main.tf
@@ -0,0 +1,27 @@
+terraform {
+    required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.16"
+    }
+}
+    required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+    region = "us-east-1"
+    profile = "user-formacao-aws" //configure iam profile
+}
+
+resource "aws_instance" "t3micro-formacao-aws" {
+    ami = "ami-20f3f602d23f1659d" //definida a imagem a ser utilizada
+    instance_type = "t3.micro"
+    tags = {
+        ambiente = "dev"
+        Name = "tag-formacao-aws-dev"
+    }
+    vpc_security_group_ids = ["sg-018ffb83d58ea0311"] //id_security
+        root_block_device {
+            volume_size = 10 //volume do disco
+        }
+}
